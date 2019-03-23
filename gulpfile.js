999cff575fb7919c540f90a87e0c970b22d5a613
Тюлеша <gryazond@gmail.com>
2019-03-23 13:00:01 +0300

    Закомитил npm i gulp-cssnano gulp-rename --save-dev и дальнейшие изменения в gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglifyjs');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 
 gulp.task('sass', async function() {
     return gulp.src('source/scss/**/*.sass')
@@ -26,6 +28,14 @@ gulp.task('libs', function() {
         .pipe(gulp.dest('source/js'));
 });
 
+gulp.task('css-libs', function() {
+    return gulp.src('source/scss/libs.sass')
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('source/css'))
+});
+
+
 gulp.task('code', function() {
     return gulp.src('source/*.html')
         .pipe(browserSync.reload({stream: true}))
@@ -40,9 +50,26 @@ gulp.task('browser-sync', function() {
         });
 });
 
+gulp.task('prebuild', async function() {
+    var buildCss = gulp.src([
+        'source/css/main.css',
+        'source/css/libs.min.css'
+    ])
+        .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('source/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('source/js/**/*')
+        .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('source/*.html')
+        .pipe(gulp.dest('dist'))
+});
+
 gulp.task('watch', function() {
     gulp.watch('source/scss/**/*.sass', gulp.parallel('sass'));
     gulp.watch('source/*.html', gulp.parallel('code'));
     gulp.watch(['source/js/script.js', 'source/libs/**/*.js'], gulp.parallel('scripts'));
 });
-gulp.task('default', gulp.parallel('sass', 'libs', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('css-libs', 'sass', 'libs', 'browser-sync', 'watch'));
